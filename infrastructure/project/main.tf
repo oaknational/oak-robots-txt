3bc21178c1db5983d75086f982aedaf75d3de538
Kofoworola Ogunleye <kay16th@yahoo.com>
2025-10-06 14:23:54 +0100

    feat: set up in vercel
    Add Vercel configuration using shared terraform module

---
+++ infrastructure/project/main.tf
@@ -0,0 +1,25 @@
+locals {
+  workspace_prefix = "oak-robots-txt-project-"
+}
+
+resource "terraform_data" "workspace_validation" {
+  lifecycle {
+    precondition {
+      condition     = startswith(terraform.workspace, local.workspace_prefix)
+      error_message = "Workspace name \"${terraform.workspace}\" must begin with ${local.workspace_prefix}"
+    }
+  }
+}
+
+module "vercel" {
+  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=v1.3.5"
+  build_type                       = "website"
+  cloudflare_zone_domain           = var.cloudflare_zone_domain
+  framework                        = null
+  project_visibility               = "public"
+  git_repo                         = "oaknational/oak-robots-txt"
+  protection_bypass_for_automation = false
+  skew_protection                  = "1 day"
+
+  domains = ["robots.thenational.academy"]
+}
