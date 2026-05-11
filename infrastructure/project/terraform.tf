4b2c0d8d6c946838b64466fcad9b6583a0e7be0b
Rob Ward <rob.ward@thenational.academy>
2026-05-11 21:50:17 +0100

    fix: oak terraform linter

--- infrastructure/project/terraform.tf
+++ infrastructure/project/terraform.tf
@@ -1,11 +1,3 @@
 terraform {
-
   required_version = ">= 1.5.7"
-
-  cloud {
-    organization = "oak-national-academy"
-    workspaces {
-      tags = ["repo:oak-robots-txt", "config:project"]
-    }
-  }
 }
